2cd775845c5db96cd964261e40dd1688f48052d9
Farid <faridahmed90297@gmail.com>
2022-01-07 18:01:24 +0530

    CompletableFuture Demo

---
+++ src/main/java/com/javatechie/async/WhyNotFuture.java
@@ -0,0 +1,42 @@
+package com.javatechie.async;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class WhyNotFuture {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    	
+    	//Diadvantage of future
+    	//multiple future can not  be chained together.
+    	//No proper Exception handling Mechanism.
+    	//cannot be manually completed.
+
+        ExecutorService service=Executors.newFixedThreadPool(10);
+
+        Future<List<Integer>> future = service.submit(() -> {
+            //your doing api call
+            System.out.println("Thread : "+Thread.currentThread().getName());
+            System.out.println(10/0);
+            return Arrays.asList(1, 2, 3, 4);
+        });
+
+
+        List<Integer> integers = future.get();
+        System.out.println(integers);
+
+        CompletableFuture<String> completableFuture=new CompletableFuture<>();
+        completableFuture.get();
+        completableFuture.complete("return some dummy data.....");
+
+    }
+
+    private static void delay(int min) {
+        try {
+            TimeUnit.MINUTES.sleep(min);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
